fc0a02041f839a04d0a047219cd6405e50df04ab
Angel Williams <angel.williams@chghealthcare.com>
2020-06-03 13:34:04 -0600

    lalalallal

--- int.spec.js
+++ int.spec.js
@@ -17,42 +17,45 @@ const consumer = kafka.consumer({ groupId: 'forager-test-runner' })
 
 describe('Testing the tester', () => {
 
+  let expectedMessage
 
   beforeAll(async () => {
     jest.useFakeTimers()
 
     await consumer.subscribe({ topic: KAFKA_TOPIC, fromBeginning: true })
-
     await consumer.connect()
-
   })
 
   afterAll(async () => {
     await consumer.disconnect()
-
   })
 
   it('should return a status code of 200 and the body should have the correct structure', async () => {
-    let expectedMessage
     await supertest(TEST_URL)
       .get(`/`)
-      // .expect('Content-Type', /json/)
-      // .expect(200)
-      .then(() => {
-        console.log("HERE")
-        // expect(typeof resp.body).toEqual('object')
-        setTimeout(async () => {
-          await consumer.run({
-            eachMessage: async ({ topic, partition, message }) => {
-              console.log("CONSUMER VALUE", {
-                value: message.value.toString(),
-              })
-              expectedMessage = message.value.toString()
-            },
-          })
-          console.log("Expected", expectedMessage)
-          expect(expectedMessage).toBe('Hello KafkaJS user!')
-        }, 5000)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then(resp => {
+        console.log("Resp", resp.body)
+        expect(typeof resp.body).toEqual('object')
       })
+  })
+
+  it('should connect to kafka and retrieve message', async () => {
+    console.log("Topic", KAFKA_TOPIC)
+
+    setTimeout(async () => {
+      await consumer.run({
+        eachMessage: async ({ KAFKA_TOPIC, partition, message }) => {
+          console.log("CONSUMER VALUE", {
+            value: message.value.toString(),
+          })
+          expectedMessage = message.value.toString()
+        },
+      })
+      console.log("Expected", expectedMessage)
+      expect(expectedMessage).toBe('Hello KafkaJS user!')
+      done()
+    }, 5000)
   })
 });
